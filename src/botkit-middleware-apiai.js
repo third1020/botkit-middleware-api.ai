e1b3397442f1955f5a6a9d1180f4b25a5dea42df
botthird <34233131+third1020@users.noreply.github.com>
2017-12-11 02:30:05 +0700

    create botkit-middleware-apiai.js

---
+++ src/botkit-middleware-apiai.js
@@ -0,0 +1,40 @@
+var apiai = require('apiai');
+
+module.exports = function(config) {
+
+    if (!config || !config.token) {
+        throw new Error('No api.ai API token specified');
+    }
+
+    var errorText = config.errorText || '';
+    var defaultText = config.defaultText || '';
+    var unknownAction = config.unknownAction || 'unknown';
+
+    var AI = apiai(config.token);
+
+    var middleware = {};
+
+    middleware.receive = function(bot, message, next) {
+        if (message.text) {
+            var request = AI.textRequest(message.text);
+            request.on('response', function(res) {
+                message.ai = res.result;
+                //console.log('res: ', res.result);
+                next();
+            });
+
+            request.on('error', function(err) {
+                console.error('Error connecting with API.AI: ', err);
+                message.ai.error = err;
+                next();
+            });
+
+            request.end();
+        }
+
+    };
+
+
+    return middleware;
+
+};
